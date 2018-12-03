f5569521318136ae616de8c182e2b8c528e7e054
tales <borenwu@163.com>
2018-12-03 22:21:27 +0800

    first commit

---
+++ Adoption.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.17;
+
+contract Adoption{
+    address[16] public adopters;
+
+    function adopt(uint petId) public returns(uint){
+        require(petId>=0 && petId<16);
+        adopters[petId] = msg.sender;
+        return petId;
+    }
+
+    function getAdopters() public view returns(address[16]){
+        return adopters;
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import Web3 from 'web3'
+import TruffleContract from 'truffle-contract'
+import AdoptionJson from './truffle/Adoption.json'
+import { Button } from 'antd';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.web3 = null
+        this.Adoption = null
+        this.init()
+        this.state = {
+
+        }
+    }
+
+    init() {
+        if (typeof window.web3 != 'undefined') {
+            this.web3Provider = window.web3.currentProvider;
+        } else {
+            alert('请安装metamask')
+        }
+        this.web3 = new Web3(this.web3Provider)
+        this.initAdoption()
+    }
+
+    initAdoption() {
+        this.Adoption = TruffleContract(AdoptionJson)
+        this.Adoption.setProvider(this.web3Provider)
+        return this.markAdopted()
+    }
+
+    async markAdopted() {
+        const adoptionInstance = await this.Adoption.deployed()
+        const adapters = await adoptionInstance.getAdopters.call()
+        console.log(adapters)
+    }
+
+    async adopt(petId){
+        const account = window.web3.eth.defaultAccount
+        const adoptionInstance = await this.Adoption.deployed()
+        await adoptionInstance.adopt(petId,{from:account})
+        this.markAdopted()
+    }
+
+    render() {
+        return <Button type="primary" onClick={()=>this.adopt(0)}>领养第0个</Button>
+    }
 }
 
-export default App;
+export default App
